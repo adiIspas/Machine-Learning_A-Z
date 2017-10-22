51b34f8713959fe94a8137ceec00338088fe7fa1
Adrian Ispas <adrian.ispas@ab4.systems>
2017-10-22 10:38:11 +0300

    Update

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_me.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_me.py
@@ -65,15 +65,18 @@ classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 3)
 
-# Part 3 - Making the predictions and evaluating the model
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
+# Save model
+classifier.save('ann_model.h5')
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-print(cm)
+# # Part 3 - Making the predictions and evaluating the model
+# # Predicting the Test set results
+# y_pred = classifier.predict(X_test)
+# y_pred = (y_pred > 0.5)
+#
+# # Making the Confusion Matrix
+# from sklearn.metrics import confusion_matrix
+# cm = confusion_matrix(y_test, y_pred)
+#
+# print(cm)
